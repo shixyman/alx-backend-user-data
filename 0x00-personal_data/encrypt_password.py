8801e2e83fa3623c899c694a599bdad5063c94c0
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-05-16 00:39:27 +0100

    final test1

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,10 +5,25 @@ Defines a hash_password function to return a hashed password
 import bcrypt
 from bcrypt import hashpw
 
-def hash_password(password):
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode(), salt)
-    return hashed_password
 
-def is_valid(hashed_password, password):
+def hash_password(password: str) -> bytes:
+    """
+    Returns a hashed password
+    Args:
+        password (str): password to be hashed
+    """
+    b = password.encode()
+    hashed = hashpw(b, bcrypt.gensalt())
+    return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Check whether a password is valid
+    Args:
+        hashed_password (bytes): hashed password
+        password (str): password in string
+    Return:
+        bool
+    """
     return bcrypt.checkpw(password.encode(), hashed_password)
--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,31 @@
+#!/usr/bin/env python3
+"""
+Definition of filter_datum function that returns an obfuscated log message
+"""
+from typing import List
 import re
 import logging
 import os
 import mysql.connector
-import bcrypt
+
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Return an obfuscated log message
+    Args:
+        fields (list): list of strings indicating fields to obfuscate
+        redaction (str): what the field will be obfuscated to
+        message (str): the log line to obfuscate
+        separator (str): the character separating the fields
+    """
+    for field in fields:
+        message = re.sub(field+'=.*?'+separator,
+                         field+'='+redaction+separator, message)
+    return message
 
 
 class RedactingFormatter(logging.Formatter):
@@ -13,66 +36,70 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
-
-    def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
-
-def filter_datum(fields, redaction, message, separator):
-    return re.sub(r'({})'.format('|'.join(map(re.escape, fields))), redaction, message).split(separator)
-
-class LogFilter:
-    def __init__(self, fields):
         self.fields = fields
 
-    def format(self, message, separator):
-        redaction = 'REDACTED'
-        filtered_fields = filter_datum(self.fields, redaction, message, separator)
-        return separator.join(filtered_fields)
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        redact the message of LogRecord instance
+        Args:
+        record (logging.LogRecord): LogRecord instance containing message
+        Return:
+            formatted string
+        """
+        message = super(RedactingFormatter, self).format(record)
+        redacted = filter_datum(self.fields, self.REDACTION,
+                                message, self.SEPARATOR)
+        return redacted
 
 
-def filter_datum(fields, redaction, message, separator):
-    return re.sub(r'({})'.format('|'.join(map(re.escape, fields))), redaction, message).split(separator)
-
-def get_logger():
-    PII_FIELDS = ('name', 'email', 'phone_number', 'address', 'social_security_number')
-
-    class RedactingFormatter(logging.Formatter):
-        def __init__(self, fields):
-            super().__init__()
-            self.fields = fields
-
-        def format(self, record):
-            message = super().format(record)
-            for field in self.fields:
-                message = message.replace(field, '[REDACTED]')
-            return message
-
-    logger = logging.getLogger('user_data')
+def get_logger() -> logging.Logger:
+    """
+    Return a logging.Logger object
+    """
+    logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
 
-    stream_handler = logging.StreamHandler()
+    handler = logging.StreamHandler()
+
     formatter = RedactingFormatter(PII_FIELDS)
-    stream_handler.setFormatter(formatter)
 
-    logger.addHandler(stream_handler)
-
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
     return logger
 
-def get_db():
-    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    database = os.getenv("PERSONAL_DATA_DB_NAME")
 
-    connection = mysql.connector.connect(
-        user=username,
-        password=password,
-        host=host,
-        database=database
-    )
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    """
+    user = os.getenv('PERSONAL_DATA_DB_USERNAME') or "root"
+    passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD') or ""
+    host = os.getenv('PERSONAL_DATA_DB_HOST') or "localhost"
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
+    conn = mysql.connector.connect(user=user,
+                                   password=passwd,
+                                   host=host,
+                                   database=db_name)
+    return conn
 
-    return connection
 
+def main():
+    """
+    main entry point
+    """
+    db = get_db()
+    logger = get_logger()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    fields = cursor.column_names
+    for row in cursor:
+        message = "".join("{}={}; ".format(k, v) for k, v in zip(fields, row))
+        logger.info(message.strip())
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
