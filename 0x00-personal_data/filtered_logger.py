65d049139a69f11b89d41d05d3f8b1a8f6023112
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-05-15 13:45:46 +0100

    test1

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,19 @@
+import re
+import logging
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
+
+def filter_datum(fields, redaction, message, separator):
+    return re.sub(r'({})'.format('|'.join(map(re.escape, fields))), redaction, message).split(separator)
