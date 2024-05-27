ec61df6b27705777b69e2fa47c7aceeca16485a4
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-05-27 14:30:51 +0100

    add code for3. update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,8 +1,11 @@
 """DB module
 """
+from typing import Dict
 from sqlalchemy import create_engine
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
 
 from user import Base, User
@@ -45,3 +48,42 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update an existing user in the database
+
+        Args:
+            user_id (int): The ID of the user to update
+            **kwargs: Keyword arguments to update the user's attributes
+
+        Raises:
+            ValueError: If an argument that does not correspond to a user attribute is passed
+        """
+        try:
+            user = self.find_user_by(id=user_id)
+        except (NoResultFound, InvalidRequestError):
+            return
+
+        for attr, value in kwargs.items():
+            if hasattr(User, attr):
+                setattr(user, attr, value)
+            else:
+                raise ValueError(f"Invalid attribute: {attr}")
+
+        self._session.commit()
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by one or more attributes
+
+        Args:
+            **kwargs: Keyword arguments to filter the user search
+
+        Returns:
+            User: The found user object
+
+        Raises:
+            NoResultFound: If no user is found
+            InvalidRequestError: If an invalid attribute is provided
+        """
+        return self._session.query(User).filter_by(**kwargs).one()
+    
